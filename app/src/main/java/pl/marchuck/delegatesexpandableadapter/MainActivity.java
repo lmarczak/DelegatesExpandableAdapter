432ae6cf8d2bbd9d04a4cde81df083473c2e60c7
Lukasz Marczak <lukmar993@gmail.com>
2016-02-16 10:57:27 +0100

    dleegates works

--- app/src/main/java/pl/marchuck/delegatesexpandableadapter/MainActivity.java
+++ app/src/main/java/pl/marchuck/delegatesexpandableadapter/MainActivity.java
@@ -29,10 +29,6 @@ import pl.marchuck.delegatesexpandableadapter.model.parent.BlueHusky;
 import pl.marchuck.delegatesexpandableadapter.model.parent.PurpleSOI;
 import pl.marchuck.delegatesexpandableadapter.model.parent.YellowYork;
 import pl.marchuck.delegatesexpandableadapter.utils.ExpandableBuilder;
-import rx.Observable;
-import rx.Subscriber;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
